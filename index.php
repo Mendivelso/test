1b7f09a204297ad09e3f1cfcff6df07b5d5aa3aa
Ronaldo <cristiangoyeneche9@gmail.com>
2020-10-05 10:22:39 -0500

    AGREGAMOS HEADER DE LA PAGINA

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-HOLA
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página web</title>
+</head>
+<body>
+    
+    <!-- Header de la pagina -->
+    <header>
+        <h1>TITULO DE LA PAGINA </h1>
+    </header>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
